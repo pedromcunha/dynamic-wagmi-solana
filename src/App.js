cee341285272d26937127bc5b4f20092c12432b4
pedromcunha <pedro.spinsomewebs@gmail.com>
2024-08-30 17:47:01 -0400

    Add solana

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { createConfig, WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { http } from "viem";
 import { mainnet } from "viem/chains";
-
 
 import Main from "./Main";
 
@@ -23,14 +23,14 @@ export default function App() {
   return (
     <DynamicContextProvider
       settings={{
-        environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
-        walletConnectors: [EthereumWalletConnectors],
+        environmentId: "",
+        walletConnectors: [EthereumWalletConnectors, SolanaWalletConnectors],
       }}
     >
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
           <DynamicWagmiConnector>
-           <Main />
+            <Main />
           </DynamicWagmiConnector>
         </QueryClientProvider>
       </WagmiProvider>
